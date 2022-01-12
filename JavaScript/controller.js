5ec5664ca947c33c4bb02b002fdbf896281c95fe
chris3evans <chris3evans.ce@gmail.com>
2022-01-12 20:17:45 +0000

    Started JS AJAX Call Logic

--- JavaScript/controller.js
+++ JavaScript/controller.js
@@ -1,3 +1,9 @@
 import * as modelObject from "./model.js";
 
-import * as mainViewObject from './Views/mainView.js';
+import * as mainViewObject from "./Views/mainView.js";
+
+const modelStockData = await modelObject.getStockData();
+const stockData = modelStockData.json();
+console.log(stockData);
+console.log(modelStockData);
+console.log(modelObject);
--- JavaScript/model.js
+++ JavaScript/model.js
@@ -1,1 +1,36 @@
-console.log("hello");
+/*https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=seOqbX0SsFtFXcPVRPL28UfKEzVFF5z8NqGpLuSz*/
+
+//seOqbX0SsFtFXcPVRPL28UfKEzVFF5z8NqGpLuSz
+// ["O", "ADM", "PG", "KO", "SPG"]
+
+const stocks = ["AAPL"];
+
+// Function to format the API string
+const adjustAPIKey = function (stockArray) {
+  const newString = stockArray.join(",");
+  return newString;
+};
+
+// Function to make an AJAX call and return an array of stock objects
+export const getStockData = async function (chosenStocks) {
+  // AJAX call to API requesting data
+  const data = await fetch(
+    `https://api.stockdata.org/v1/data/quote?symbols=${chosenStocks}&api_token=seOqbX0SsFtFXcPVRPL28UfKEzVFF5z8NqGpLuSz`
+  );
+
+  // Object containing array of all requested stocks
+  const dataObject = await data.json();
+
+  // Array containing one object of data for each stock that was requested
+  const dataArray = dataObject.data;
+  return dataArray;
+};
+
+// Formatted string ready to be used in the API call
+const APIString = adjustAPIKey(stocks);
+
+// An array of stocks, one object for each share stock requested
+export const stocksArray = getStockData(APIString).then(function (data) {
+  console.log(data);
+  return data;
+});
